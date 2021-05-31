22be62dbcba8c8af639e3a4d4fb9b65ee93ea757
Tammy <tammycwong@gmail.com>
2021-05-31 16:28:15 -0400

    fetch

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -1,16 +1,29 @@
 import React from 'react'
 
- function PetCard({pet}) {
+ function PetCard({showPet}) {
+    //  const [pets, setPets] = useState("")
 
+    function handleDelete(id) {
+        fetch(`http://localhost:3000/pets${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type" : "application/json",
+                Authorization: `Bearer ${localStorage.token}`
+            }
+        })
+            .then(r =>r.json())
+            .then(r => console.log(r))
+    }
 
     return (
         <div>
             <ul>
             <li className="card">
-                <h4>{pet.name}</h4>
-                <p>{pet.swag_total}</p>
-                <p>{pet.user_id}</p>
-                <img src = {pet.image} alt="name" />
+                <h4>{showPet.name}</h4>
+                <p>{showPet.swag_total}</p>
+                <p>{showPet.user_id}</p>
+                <img src = {showPet.image} alt="name" />
+                <button onClick= {handleDelete}>Delete</button>
             </li>
             </ul>
         </div>
